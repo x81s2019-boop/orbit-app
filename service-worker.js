170b1aa8e901a9092d95ed8d0cf98c00bb68b10a
x81s2019-boop <x81s2019@gmail.com>
2025-12-09 12:56:06 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+// Orbit PWA Service Worker - 簡化版
+const CACHE_NAME = 'orbit-cache-v1.0.0';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// 安裝
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+// 激活
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+// 攔截請求
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
+
